1c8f9fb48d0ae5ea6e2fd779736dbc04c4a6718a
slackroad <artpogosian@gmail.com>
2015-01-10 21:46:59 -0500

    v1.05 website (announcement)

--- myscript.js
+++ myscript.js
@@ -10,3 +10,5 @@ function startGame(string){
 		$(".content").append('<object width="800" height="600"> <param name="movie" value="' + string + '"> <embed class="game" src="' + string + '" width="700" height="500"> </embed> </object>');
 	}
 }
+
+//Im too hot
